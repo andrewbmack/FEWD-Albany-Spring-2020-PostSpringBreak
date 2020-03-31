ec9ee28437abf6afe0d404beb933da0c61ed05c8
andrewbmack <andrewbmack@gmail.com>
2020-03-30 21:22:52 -0400

    andrews array01

--- exercises/01.arrays.js
+++ exercises/01.arrays.js
@@ -10,19 +10,19 @@
  * @description
  * create an arrays of names
  */
-
+let names = ['andrew', 'melinda', 'lucas', 'amelie', 'emmeline'];
 console.log(names);
 
 /**
  * @description
  * access the first item in the array above
  */
-
+let first = names[0]
 /**
  * @description
  * access the last character in the first item in the array above
  */
-
+console.log(first.charAt(0))
 /**
  * @do
  * include this file in a new commit and push 
